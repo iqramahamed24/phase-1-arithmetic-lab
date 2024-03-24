74f5a49ba306ec2197a7f2d0d77a81d53ba45f85
iqramahamed <iqra.mahamed64@gmail.com>
2024-03-24 18:02:29 +0300

    go

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() *10)
+
+let num3 = 8
+let num4 = 2
+
+let mod = num3 / num4
+
+let max = Math.max(5, 10, 15, 20,)
+
+
+
+
 
 // Write your code here
